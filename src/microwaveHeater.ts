2ec73c90a02af35d2db8c62204b5ec1fbe823a3a
JacksonNaufal <JacksonNaufal@users.noreply.github.com>
2022-09-20 00:30:22 +0000

    Prettified Code!

--- src/microwaveHeater.ts
+++ src/microwaveHeater.ts
@@ -1,5 +1,5 @@
 /**
- * The program is the classic 
+ * The program is the classic
  * microwave.
  *
  * By:      Jackson Naufal
@@ -22,34 +22,35 @@ let time = 0
 let minutes = 0
 let seconds = 0
 
-
 // let userInput = soup, pizza, or sub
 const userInput = prompt('Enter your choice, sub, pizza or soup: ')
 if (userInput === 'sub' || userInput === 'pizza' || userInput === 'soup') {
   const timeString = prompt('Enter your amount: ')
   const amountNumber = parseFloat(timeString)
 
-    if (userInput === 'sub') {
-     time = subTime
-    } else if (userInput === 'pizza') {
-     time = pizzaTime
-    } else if (userInput === 'soup') {
-     time = soupTime
+  if (userInput === 'sub') {
+    time = subTime
+  } else if (userInput === 'pizza') {
+    time = pizzaTime
+  } else if (userInput === 'soup') {
+    time = soupTime
+  }
+
+  if (amountNumber == 1 || amountNumber == 2 || amountNumber == 3) {
+    if (amountNumber == 2) {
+      time = time * two
+    } else if (amountNumber == 3) {
+      time = time * three
     }
-	
-    if (amountNumber == 1 || amountNumber == 2 || amountNumber == 3) {
-        if (amountNumber == 2) {
-	  time = time * two
-        } else if (amountNumber == 3) {
-	  time = time * three
-        }
-	seconds = (time / 60)
-	minutes = Math.floor(seconds)
-	seconds = (seconds - minutes) * 60
-	console.log(`The ${userInput} will be done in ${minutes} minutes ${seconds} seconds!`)
-    } else {
-	console.log('Invalid Number!')
-    }
+    seconds = time / 60
+    minutes = Math.floor(seconds)
+    seconds = (seconds - minutes) * 60
+    console.log(
+      `The ${userInput} will be done in ${minutes} minutes ${seconds} seconds!`
+    )
+  } else {
+    console.log('Invalid Number!')
+  }
 } else {
   // catch if an input is invalid
   console.log('Invalid Input')
