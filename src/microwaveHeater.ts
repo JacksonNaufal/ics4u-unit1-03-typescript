274b2c146bbce17922f41553cb344a31d40cb42a
Jackson Naufal <Jackson.Naufal@mths.ca>
2022-09-22 00:11:39 +0000

    fixed

--- src/microwaveHeater.ts
+++ src/microwaveHeater.ts
@@ -4,13 +4,14 @@
  *
  * By:      Jackson Naufal
  * Version: 1.0
- * Since:   2020-01-01
+ * Since:   2020-09-20
  */
 
 import promptSync from 'prompt-sync'
 
 const prompt = promptSync()
 
+// These are my constants for my program
 const subTime = 60
 const pizzaTime = 45
 const soupTime = 105
@@ -22,10 +23,13 @@ let seconds = 0
 
 // let userInput = soup, pizza, or sub
 const userInput = prompt('Enter your choice, sub, pizza or soup: ')
+
+// this checks if the user inputted a valid item
 if (userInput === 'sub' || userInput === 'pizza' || userInput === 'soup') {
   const timeString = prompt('Enter your amount: ')
   const amountNumber = parseFloat(timeString)
 
+  // this checks what the user inputted
   if (userInput === 'sub') {
     time = subTime
   } else if (userInput === 'pizza') {
@@ -34,23 +38,29 @@ if (userInput === 'sub' || userInput === 'pizza' || userInput === 'soup') {
     time = soupTime
   }
 
-  if (amountNumber == 1 || amountNumber == 2 || amountNumber == 3) {
-    if (amountNumber == 2) {
+  // this checks the amount of what the user inputted
+  if (amountNumber === 1 || amountNumber === 2 || amountNumber === 3) {
+    if (amountNumber === 2) {
       time = time * two
-    } else if (amountNumber == 3) {
+    } else if (amountNumber === 3) {
       time = time * three
     }
+
+    // this converts the time to minutes and seconds
     seconds = time / 60
     minutes = Math.floor(seconds)
     seconds = (seconds - minutes) * 60
     console.log(
       `The ${userInput} will be done in ${minutes} minutes ${seconds} seconds!`
     )
+
+    // this catches invalid number inputs
   } else {
     console.log('Invalid Number!')
   }
+
+  // this catches invalid food inputs
 } else {
-  // catch if an input is invalid
   console.log('Invalid Input')
 }
 console.log('\nDone.')
